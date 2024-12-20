e6ae3bdb92b9f7298193b099f16150d54c214117
asilbek <asilbekmirolimov@gmail.com>
2024-12-20 09:32:00 +0500

    first commit

---
+++ states.py
@@ -0,0 +1,21 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+class HodimState(StatesGroup):
+    idora = State()
+    texnologiya = State()
+    telefon = State()
+    hudud = State()
+    ism_familiya = State()
+    murojaat_vaqti = State()
+    ish_vaqti = State()
+    maosh = State()
+    qoshimcha = State()
+    ha_yoq = State()
+
+
+class IshJoyiState():
+    pass
+
